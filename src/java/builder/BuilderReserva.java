53045da82ea6710535ec6b2a265336c15fbae01b
Edmiltho <edmiltho@hotmail.com>
2018-10-11 08:15:30 -0300

    Padrão builder
    Aplicação do builder nas classes Estacionamento e Reserva

---
+++ src/java/builder/BuilderReserva.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+
+import modelo.componentes.Vaga;
+import modelo.componentes.Veiculo;
+import modelo.operacoes.Pagamento;
+import modelo.operacoes.Reserva;
+import modelo.operadores.Motorista;
+import modelo.utilidade.ClasseRelacoes;
+
+/**
+ *
+ * @author Cacherow
+ */
+public class BuilderReserva {
+    private Reserva reserva;
+    private Pagamento pagamento;
+    private Veiculo veiculo;
+    private Vaga vaga;
+
+    public BuilderReserva(Reserva reserva, Pagamento pagamento, Veiculo veiculo, Vaga vaga) {
+        this.reserva = reserva;
+        this.pagamento = pagamento;
+        this.veiculo = veiculo;
+        this.vaga = vaga;
+    }
+
+    
+    public void addReserva(String horaReserva, String dataReserva, String horaSaida){
+        Reserva reserva = new Reserva();
+        reserva.setDataReserva(dataReserva);
+        reserva.setHoraReserva(horaReserva);
+        reserva.setHoraSaida(horaSaida);
+    }
+    
+    //Adiciona os atributos de Pagamento, relaciona cnh com um motorista existente e adiciona Pagamento à reserva
+    public void addPagamento(double valor, String data, boolean status, String cnh){
+        Pagamento pagamento = new Pagamento();
+        pagamento.setValor(valor);
+        pagamento.setData(data);
+        pagamento.setStatus(status);
+        
+        Motorista motorista = ClasseRelacoes.getMotorista(cnh);
+        pagamento.setMotorista(motorista);
+        
+        reserva.setPagamento(pagamento);
+    }
+    
+    //Adiciona os atributos de veiculo e relaciona o motorista do veiculo
+    public void addVeiculo(String placa, int ano, String modelo, String marca, String cor, String cnh){
+        Veiculo veiculo =  new Veiculo();
+        veiculo.setPlaca(placa);
+        veiculo.setAno(ano);
+        veiculo.setCor(cor);
+        veiculo.setMarca(marca);
+        veiculo.setModelo(modelo);
+        
+        Motorista motorista = ClasseRelacoes.getMotorista(cnh);
+        veiculo.setMotorista(motorista);
+        
+        reserva.setVeiculo(veiculo);
+    }
+    
+    //Relaciona a vaga à reserva
+    public void addVaga(int numero){
+        Vaga vaga = ClasseRelacoes.getVaga(numero);
+        reserva.setVaga(vaga);
+    }
+}
---
+++ src/java/builder/DiretorReserva.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+import modelo.componentes.Vaga;
+import modelo.componentes.Veiculo;
+import modelo.operacoes.Pagamento;
+import modelo.operacoes.Reserva;
+import modelo.operadores.Motorista;
+import modelo.utilidade.ClasseRelacoes;
+/**
+ *
+ * @author Cacherow
+ */
+public class DiretorReserva {
+    public DiretorReserva(){}
+    public Reserva builder(Pagamento pagamento, Veiculo veiculo, Vaga vaga, String horaReserva, String dataReserva, String horaSaida,double valor, String data, boolean status, String cnh,String placa, int ano, String modelo, String marca, String cor, int numero)throws Exception{
+        Reserva reserva = new Reserva();
+        BuilderReserva BR = new BuilderReserva(reserva, pagamento, veiculo, vaga);
+        BR.addReserva(horaReserva, dataReserva, horaSaida);
+        BR.addPagamento(valor, data, status, cnh);
+        BR.addVeiculo(placa, ano, modelo, marca, cor, cnh);
+        BR.addVaga(numero);
+        return reserva;  
+    }
+}
