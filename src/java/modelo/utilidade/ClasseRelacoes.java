b3b91d79349d094f137fb001d05b57a1b24b185b
Edmiltho <edmiltho@hotmail.com>
2018-10-11 08:17:53 -0300

    Classe Relacoes

---
+++ src/java/modelo/utilidade/ClasseRelacoes.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.utilidade;
+
+import java.util.ArrayList;
+import modelo.componentes.Vaga;
+import modelo.operadores.Funcionario;
+import modelo.operadores.Motorista;
+
+/**
+ *
+ * @author 20131BSI0084
+ */
+public class ClasseRelacoes {
+    
+    //private static ArrayList<Vaga> listaSensor;
+    private static int[] listaSensor;
+    private static ArrayList<Funcionario> listaFuncionarios;
+    private static ArrayList<Motorista> listaMotoristas;
+    private static ArrayList<Vaga> listaVagas;
+
+    public ClasseRelacoes() {
+    }
+
+    
+    
+    public static ArrayList<Vaga> retornaVagas(int[] listaSensor){
+        ArrayList<Vaga> novasVagas = new ArrayList<Vaga>(){};
+        for(int i=0;i<listaSensor.length;i++){
+            Vaga vaga = new Vaga();
+            vaga.setNumero(listaSensor[i]);
+            novasVagas.add(vaga);
+        }
+        return novasVagas;
+    }
+    
+    
+     public static void addFuncionario(Funcionario funcionario){
+         ClasseRelacoes.listaFuncionarios.add(funcionario);         
+     }
+     
+     public static Motorista getMotorista(String cnh){
+        int i = 0;
+        for(;cnh != listaMotoristas.get(i).getCnh() || i<listaMotoristas.size() ;i++){}
+        if (cnh == listaMotoristas.get(i).getCnh()){
+                return listaMotoristas.get(i);
+        }
+        else{
+            return null;
+        }
+    }
+     
+    public static Vaga getVaga(int numero){
+        int i = 0;
+        for(;numero != listaVagas.get(i).getNumero() || i<listaVagas.size() ;i++){}
+        if (numero == listaVagas.get(i).getNumero()){
+                return listaVagas.get(i);
+        }
+        else{
+            return null;
+        }
+    }
+
+     public static Funcionario getFuncionario(String matricula){
+        int i = 0;
+        for(;matricula != listaFuncionarios.get(i).getMatricula() || i<listaFuncionarios.size() ;i++){}
+        if (matricula == listaFuncionarios.get(i).getMatricula()){
+                return listaFuncionarios.get(i);
+        }
+        else{
+            return null;
+        }
+    }
+}
