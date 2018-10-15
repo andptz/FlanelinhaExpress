9659d052a274ffccfc8bc82361706361c8d63f63
Caio Pupolin <pupolinantoniocaio@gmail.com>
2018-10-15 10:50:40 -0300

    a
    a

--- src/java/modelo/operadores/Guarda.java
+++ src/java/modelo/operadores/Guarda.java
@@ -5,8 +5,5 @@ public class Guarda extends Funcionario {
     
     public Guarda(int id, String nomeCompleto, String email, String telefone,String senha, String cpf, String matricula, String dataAdmissao, String dataDemissao) {
         super(id, nomeCompleto, email, telefone,senha, cpf, matricula, dataAdmissao, dataDemissao);
-    }
-    
-  
-    
+    }  
 }
