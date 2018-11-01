c7c1df3144c28e4d172204c6ee349eb44907fc6f
emanuelampinelli <emanue_rampinelli@hotmail.com>
2018-11-01 10:18:11 -0200

    .

--- src/java/padroes/criacao/builder/BuilderReserva.java
+++ src/java/padroes/criacao/builder/BuilderReserva.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package builder;
+package padroes.criacao.builder;
 
 import modelo.componentes.Vaga;
 import modelo.componentes.Veiculo;
