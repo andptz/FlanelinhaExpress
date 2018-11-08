50dea63e5821d50c06bd9e2f5aeab430f6b61189
Emanuel Rampinelli <37427276+emanuelrampinelli@users.noreply.github.com>
2018-11-08 09:33:36 -0200

    UP

---
+++ src/java/paginasWebControl/PagCadastro.java
@@ -0,0 +1,48 @@
+package paginasWebControl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class PagCadastro extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+           
+         
+               response.sendRedirect("pagPosLogin/generic.html");
+            
+        } finally {
+            out.close();
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    public String getServletInfo() {
+        return "Servlet Recebe Dados";
+    }// </editor-fold>
+
+}
---
+++ src/java/paginasWebControl/PagHome.java
@@ -0,0 +1,7 @@
+
+package paginasWebControl;
+
+
+public class PagHome {
+    
+}
